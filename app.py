0b42f213a0172ec5a5c6ecd5cd3f4db7817f3eff
Акылай <sultanbekovaakylaj90@gmail.com>
2020-11-19 14:02:04 +0600

    added title and base to excel

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
 from flask import Flask, render_template, request
-
+from openpyxl import load_workbook
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-     f = open("urls.txt", "r", encoding="utf-8")
-     u = [row for row in f]
-     f.close
-     return render_template("index.html", urls=u)
+     
+     #мы делим строку на [описание, ссылка] картинки
+     images = []
+     excel = load_workbook("gallery.xlsx")
+     page = excel["Лист1"]
+     for row in page:
+          url = row[0].value
+          description = row[1].value
+          title = row[2].value
+          lst = [title, description, url]
+          images.append(lst)
+
+     return render_template("index.html", images=images)
 
 
 @app.route("/add")
 def add():
-     
-    return render_template ("form.html")
+     return render_template ("form.html")
 
 
 @app.route("/reciever", methods=["POST"])
 def reciever():
+     description = request.form.get("description")
      url = request.form.get("url")
-     f = open("urls.txt", "a+", encoding="utf=8")
-     f.write(url + "\n")
-     sources = [row for row in f]
-     f.close()
-     return render_template("index.html", urls=sources)
+     title = request.form.get("title")
+
+     excel = load_workbook("gallery.xlsx")
+     sheet = excel["Лист1"]
+     sheet.append([url, description, title])
+     excel.save("gallery.xlsx")
+
+     return render_template("form.html")
